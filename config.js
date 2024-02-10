0a679a3a7cd9570ec0747968271ee7ce9e38abcd
adrian <adrianlin92@gmail.com>
2024-02-10 23:51:24 +1100

    removed messages

--- config.js
+++ config.js
@@ -11,16 +11,16 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'Adrian',
-	imageBackground: false,
-	openInNewTab: true,
-	twelveHourFormat: false,
+	//name: 'Adrian',
+	//imageBackground: false,
+	//openInNewTab: true,
+	//twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	//greetingMorning: 'Good morning!',
+	//greetingAfternoon: 'Good afternoon,',
+	//greetingEvening: 'Good evening,',
+	//greetingNight: 'Go to Sleep!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
